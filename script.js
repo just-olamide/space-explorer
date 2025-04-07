e58a319208bdb60b1b79cda19fb42fd03d80069e
zaynab <holamideh8@gmail.com>
2025-04-07 22:26:01 +0100

    integrating

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ const planetLog = [
 {name: "Venus", distance: 66, explored: false, type:"Gas"},
 {name: "Mars", distance: 40, explored: true, type:"Ice"},
 {name: "Jupiter", distance: 77, explored: false, type:"Rocky"},
-{name: "Saturn", distance: 60, explored: false, type:"Gas"},
+{name: "", distance: 60, explored: false, type:"Gas"},
 
 ]
 
 console.table(planetLog);
 
+// step2
+
+for (i = 0; i < planetLog.length; i++){
+    if (planetLog[i].explored == true){
+        console.log(` Explored: ${planetLog[i].name} - ${planetLog[i].type}`);
+    }
+        else {
+            console.log(` Uncharted: ${planetLog[i].name} - ${planetLog[i].distance} M Km away`)
+        }
+        if (planetLog[i].distance < 100){
+            console.log(` ${planetLog[i].name} - Nearby!`);
+            
+        }
+        
+}function validatePlanet(planet) {
+    if (typeof planet.name == null || typeof planet.name == undefined){
+        return;
+    }
+    if (planetLog.missing == true){
+        console.log(`Error: ${planetLog.name} has incomplete data `);
+        
+    }
+}
