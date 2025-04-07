0b080d810c50dfc4fa98f46b1c882eeaa724d1f3
zaynab <holamideh8@gmail.com>
2025-04-07 21:19:06 +0100

    genesis

---
+++ script.js
@@ -0,0 +1,11 @@
+const planetLog = [
+{name: "Mercury", distance: 57, explored: true, type:"Rocky"},
+{name: "Venus", distance: 66, explored: false, type:"Gas"},
+{name: "Mars", distance: 40, explored: true, type:"Ice"},
+{name: "Jupiter", distance: 77, explored: false, type:"Rocky"},
+{name: "Saturn", distance: 60, explored: false, type:"Gas"},
+
+]
+
+console.table(planetLog);
+
